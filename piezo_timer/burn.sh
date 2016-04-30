03c0bcc56846920d1b195f23fdb937c8010122e6
mick909 <mick.neumann.0313.1030@gmail.com>
2016-04-30 18:56:49 +0900

    modify

--- piezo_timer/burn.sh
+++ piezo_timer/burn.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 avrdude -C ../../avrdude.conf -c avrispmkII -p m328p -P usb -e -v -b 19200
-avrdude -C ../../avrdude.conf -c avrispmkII -p m328p -P usb -U lfuse:w:0xf7:m -U hfuse:w:0xdf:m -U efuse:w:0x07:m -v -b 19200
+avrdude -C ../../avrdude.conf -c avrispmkII -p m328p -P usb -U lfuse:w:0xf7:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m -v -b 19200
 avrdude -C ../../avrdude.conf -c avrispmkII -p m328p -P usb -U flash:w:obj/shottimer.hex -v -b 19200
 avrdude -C ../../avrdude.conf -c avrispmkII -p m328p -P usb -U lock:w:0x00:m -v -b 19200
 
