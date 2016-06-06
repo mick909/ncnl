950abfe0ab298e98d796be794b6e3323deb3f81e
mick909 <mick.neumann.0313.1030@gmail.com>
2016-06-06 19:25:57 +0900

    change SPI mode to 0

--- spi_drive_7seg/main.c
+++ spi_drive_7seg/main.c
@@ -3,9 +3,6 @@
 #include <avr/sleep.h>
 #include <avr/wdt.h>
 
-#define SCK 5
-#define RCK 4
-#define DATA 3
 
 /*         0 1 2 3 4 5 6 7 8 9
           ---------------------
@@ -26,14 +23,14 @@ EMPTY_INTERRUPT(WDT_vect);
 void init_spi_led(void)
 {
 /* Clear 74HC595 */
-	SPCR  = 0b01011100;	/* SPIE=0, SPE=1, MSTR=1, CPOL=SPHA=0, SPR=osc/2 */
+	SPCR  = 0b01010000;	/* SPIE=0, SPE=1, MSTR=1, CPOL=SPHA=0, SPR=osc/2 */
 	SPSR  = 0b00000001;	/* SPI2X=1 */
 
 	SPDR  = 0xff;
 	do {} while ( !(SPSR & 0b10000000) );
 	SPCR = 0;			/* SPI Disable */
 
-/* Clear 74HC164 */
+/* Clear 74AC164 */
 	PORTB &= ~0b00001000;
 	PINB  = 0b00010000;	/* MISO output L */
 	PINB  = 0b00010000;	/* MISO output H -> load */
@@ -48,14 +45,14 @@ void init_spi_led(void)
 
 void output_spi_led(uint8_t d, uint8_t f)
 {
-	SPCR  = 0b01011100;	/* SPIE=0, SPE=1, MSTR=1, CPOL=SPHA=0, SPR=osc/2 */
+	SPCR  = 0b01010000;	/* SPIE=0, SPE=1, MSTR=1, CPOL=SPHA=0, SPR=osc/2 */
 	SPSR  = 0b00000001;	/* SPI2X=1 */
 
 	SPDR  = d;
 	do {} while ( !(SPSR & 0b10000000) );
 	SPCR = 0;			/* SPI Disable */
 
-	PORTB &= ~0b00001000;			/* setup MOSI as 74HC164's data */
+	PORTB &= ~0b00001000;			/* setup MOSI as 74AC164's data */
 	if (f) PORTB |= 0b00001000;
 
 	PINB  = 0b00010000;	/* MISO output H */
@@ -66,13 +63,13 @@ int main(void)
 {
 	// PB7 : XTAL2
 	// PB6 : XTAL1
-	// PB5 : SCK  - 74HC595:SCK                / Output, High
-	// PB4 : MISO - 74HC595:RCK,G  74HC164:CLK / Output, High, (& PullDown)
-	// PB3 : MOSI - 74HC595:SI     74HC164:A,B / Output, Low
+	// PB5 : SCK  - 74HC595:SCK                / Output, Low
+	// PB4 : MISO - 74HC595:RCK,G  74AC164:CLK / Output, High, (& PullDown)
+	// PB3 : MOSI - 74HC595:SI     74AC164:A,B / Output, Low
 	// PB2 : SS   - Output, High ... not use
 	// PB1 : x    - Input, PU
 	// PB0 : x    - Input, PU
-	PORTB = 0b00111111;
+	PORTB = 0b00011111;
 	DDRB  = 0b00111100;
 
 	PORTC = 0b00111111;
@@ -81,7 +78,12 @@ int main(void)
 	PORTD = 0b11111111;
 	DDRD  = 0b00000000;
 
+	/* Before init, SCK:Output-L, MISO:Output-H, MOSI:Output-L */
+	/* 74HC595 is disabled by G(MISO)-H */
 	init_spi_led();
+	/* Now, SCK:Output-L, MISO:Output-L, MOSI:Output-L */
+	/* 74HC595 Latching all H */
+	/* 74AC164 Latching all L */
 
 	do {
 		output_spi_led(digit[8], 1);
