a718ee9b859a6f129bf4555b5f2e65a9a580249d
mick909 <mick.neumann.0313.1030@gmail.com>
2016-09-13 06:55:06 +0900

    fix burn.sh

--- led_sensor/burn.sh
+++ led_sensor/burn.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-avrdude -C ../../avrdude.conf -c avrispmkII -p t13 -P usb -U lfuse:w:0x6b:m -v -b 2152
-
+avrdude -C ../../avrdude.conf -c avrispmkII -p t13 -P usb -e -v -b 2152
+avrdude -C ../../avrdude.conf -c avrispmkII -p t13 -P usb -U hfuse:w:0xff:m -U lfuse:w:0x6b:m -v -b 2152
 avrdude -C ../../avrdude.conf -c avrispmkII -p t13 -P usb -U flash:w:obj/led_sensor.hex -v -b 2152
-
-avrdude -C ../../avrdude.conf -c avrispmkII -p t13 -P usb -U lock:w:0x3c:m -v -b 2152
+avrdude -C ../../avrdude.conf -c avrispmkII -p t13 -P usb -U lock:w:0xfc:m -v -b 2152
